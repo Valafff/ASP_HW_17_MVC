012ae171957b125546b9a82eb273b6138a16158b
Vetal <Vetal@GB550PRO>
2024-12-15 19:59:06 +0300

    Add project files.

---
+++ HW_17_MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson_17_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    ////GetPost
+    //public class HomeController : Controller
+    //{
+    //    [HttpGet]
+    //    public string Index()
+    //    {
+    //        return "Hello world";
+    //    }
+    //    ////Тоже самое
+    //    //[HttpGet]
+    //    //public string Index() => "Hello world";
+
+
+    //    [HttpPost]
+    //    public string Hello() => "Hello ASP.NET";
+
+    //}
+}
---
+++ HW_17_MVC/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lesson_17_MVC.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
